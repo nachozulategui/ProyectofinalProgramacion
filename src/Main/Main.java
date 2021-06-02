9973c52f81faa591ebb0f6cb6952e185ee9bca42
nacho <nachozc69@gmail.com>
2021-06-02 17:22:08 +0200

    cambios en main y ventana correctos

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,10 @@
 package Main;
-
+import vista.Ventana;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new Ventana();
 	}
 
 }
--- src/vista/Ventana.java
+++ src/vista/Ventana.java
@@ -11,25 +11,6 @@ public class Ventana extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Ventana frame = new Ventana();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -37,6 +18,8 @@ public class Ventana extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		setVisible(true);
 	}
 
 }
