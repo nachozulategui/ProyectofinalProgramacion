e766d2dd3eb8aa78a0b0b9f294c7b6e05bc9e2ec
nacho <nachozc69@gmail.com>
2021-06-07 16:48:21 +0200

    bbdd.ventana

--- src/vista/Ventana.java
+++ src/vista/Ventana.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ventana extends JFrame {
 
@@ -16,10 +19,17 @@ public class Ventana extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(162, 145, 85, 21);
+		contentPane.add(btnNewButton);
 		
 		setVisible(true);
 	}
-
 }
